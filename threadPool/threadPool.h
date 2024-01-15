5fa63c5a1dbd902cc55ef79bac71d764a0ce2f44
yexinran1 <yexinran96@163.com>
2024-01-15 14:28:54 +0800

    [新增]新增线程池结构体

---
+++ threadPool/threadPool.h
@@ -0,0 +1,18 @@
+#ifndef __THREAD_POOL_H_
+#define __THREAD_POOL_H_
+#include<pthread.h>
+
+/*线程池结构体*/
+typedef struct threadpool_t
+{
+    /* 线程池中的线程 */
+    pthread_t * threadId;
+    /*最小线程数*/
+    int minThreads;
+    /*最大线程数*/
+    int maxThreads;
+    /**/
+
+}threadpool_t;
+
+#endif
